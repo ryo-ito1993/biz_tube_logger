a84a5dc75c84a09fcb5ef2ac99df3806d2a3270c
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-08 21:55:44 +0900

    Add:vue.js3の導入、Hello_vue.jsを表示させる

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,27 @@
 const { environment } = require('@rails/webpacker')
 
+const { VueLoaderPlugin } = require('vue-loader')
+
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue',{
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
+
+const { DefinePlugin } = require('webpack')
+environment.plugins.prepend(
+    'Define',
+    new DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: true
+    })
+)
+
+
 module.exports = environment
