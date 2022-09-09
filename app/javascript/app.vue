f156e8726e195ea6ea96e9edba8bd9f8b8259f78
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-09 11:34:44 +0900

    Add:vue-routerで/ページにTopIndexページを表示、vuetifyのアイコンが読み込めるよう設定

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,15 @@
 <template>
-  <v-app id="app">
-    <v-btn>Vuetifyのボタン</v-btn>    <p>{{ message }}</p>
+  <v-app>
+    <v-content>
+      <router-view />
+    </v-content>
   </v-app>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
+
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+
 </style>
---
+++ app/javascript/pages/top/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <v-card width="400px" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">ログイン</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-icon></v-icon>
+        <v-text-field prepend-icon="mdi-account-circle" label="ユーザー名" />
+        <v-text-field prepend-icon="mdi-lock"  label="パスワード" />
+        <v-card-actions>
+          <v-btn class="info">ログイン</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+  </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopIndex",
+  data() {
+    return {
+      message: "hello,vue"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
