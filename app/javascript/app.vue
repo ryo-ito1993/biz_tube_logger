91ce3a206f4355d011682877527119cb89491d74
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-10 10:39:16 +0900

    Add:ヘッダーとフッターを設置

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,23 @@
 <template>
   <v-app>
-    <v-content>
+    <TheHeader />
+    <v-content class="blue">
       <router-view />
     </v-content>
+    <TheFooter />
   </v-app>
 </template>
 
 <script>
+import TheHeader from "components/TheHeader"
+import TheFooter from "components/TheFooter"
 
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
 </script>
 
 <style scoped>
---
+++ app/javascript/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar color="pink" dark app height="70">
+    <v-toolbar-title>BizTubeLogger</v-toolbar-title>
+    <div class="flex-grow-1"></div>
+    <v-btn class="mr-5">
+      投稿一覧
+    </v-btn>
+    <v-btn class="mr-5">
+      新規登録
+    </v-btn>
+    <v-btn>
+      ログイン
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
+
+<style scoped>
+</style>
