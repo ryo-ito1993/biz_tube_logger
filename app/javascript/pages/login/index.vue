ae27cf659c56eb3a6e9ce74888cad321a92c9ca8
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-11 17:01:52 +0900

    Add:ログイン画面の作成

--- app/javascript/pages/login/index.vue
+++ app/javascript/pages/login/index.vue
@@ -1,5 +1,59 @@
 <template>
-  <h1>
-    hogeeee
-  </h1>
+  <v-card
+    width="450px"
+    class="mx-auto mt-5"
+  >
+    <v-card-title>
+      <h1 class="display-1">
+        ログイン
+      </h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="user.email"
+          label="メールアドレス"
+          type="email"
+          prepend-icon="mdi-email"
+        />
+        <v-text-field
+          v-model="user.password"
+          label="パスワード"
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+        <v-card-actions>
+          <v-btn
+            class="info"
+          >
+            ログイン
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
+
+<script>
+export default {
+  name: "LoginIndex",
+  data () {
+    return {
+      showPassword: false,
+      user: {
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
