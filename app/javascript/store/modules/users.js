16ffdbcd09cf6cb5625c69c7cc58713ee5f72788
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-12 09:13:38 +0900

    Fix:不要な記述の削除

--- app/javascript/store/modules/users.js
+++ app/javascript/store/modules/users.js
@@ -16,17 +16,14 @@ const mutations = {
 
 const actions = {
   async loginUser({ commit }, user) {
-    // ログイン
     const sessionsResponse = await axios.post('sessions', user)
     localStorage.auth_token = sessionsResponse.data.token
     axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.auth_token}`
 
-    // ログインユーザー情報の取得
     const userResponse = await axios.get('users/me')
     commit('setUser', userResponse.data)
   },
   logoutUser({ commit }) {
-    // ログアウト
     localStorage.removeItem('auth_token')
     axios.defaults.headers.common['Authorization'] = ''
     commit('setUser', null)
