9180c1cb73719c6f9b1e179201ddb6a4d8144798
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-23 16:27:49 +0900

    Add:bookmarkモデルの作成、アソシエーションの定義

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :video
+
+  validates :user_id, uniqueness: { scope: :video_id }
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :videos, dependent: :destroy
   has_many :outputs, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_videos, through: :bookmarks, source: :video
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
@@ -12,4 +14,13 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
+
+  def bookmark(video)
+    bookmark_videos << video
+  end
+
+  def unbookmark(video)
+    bookmark_videos.destroy(video)
+  end
+
 end
