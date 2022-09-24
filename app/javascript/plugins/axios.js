a8ff06dfd8f4bf0cd06818e20b504c39d0331be3
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-25 00:14:05 +0900

    Fix:axiosのbaseURLを変更

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,8 +1,16 @@
 import axios from 'axios'
 
+let baseUrl ;
+if ( process.env.NODE_ENV == 'production' ) {
+  baseUrl = 'https://biztube-logger.herokuapp.com/api'
+} else {
+  baseUrl = 'http://localhost:3000/api/'
+}
+
 const axiosInstance = axios.create({
-  baseURL: `${process.env.VUE_APP_API_ORIGIN}/api/`
+  baseURL: baseUrl
 })
+
 
 if (localStorage.auth_token) {
   axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.auth_token}`
