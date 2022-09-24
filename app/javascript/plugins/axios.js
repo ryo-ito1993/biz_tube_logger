fe87c6b330a9d617f9fbdef3780339899123f95a
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-24 23:57:31 +0900

    Add:baseURLの設定変更

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/'
+  baseURL: `${process.env.VUE_APP_API_ORIGIN}/api/`
 })
 
 if (localStorage.auth_token) {
