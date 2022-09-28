fe3f35dc6b65c13d444a61cc34fd06bea6ad90e4
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-28 15:11:55 +0900

    Fix:本番環境でエラーが発生したため、リダイレクト処理を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,4 @@ class ApplicationController < ActionController::Base
   include Api::UserAuthenticator
   protect_from_forgery with: :null_session
 
-  #herokuapp.comから独自ドメインへリダイレクト
-  before_filter :ensure_domain
-  FQDN = 'www.biztube-logger.net'
-
-  # redirect correct server from herokuapp domain for SEO
-  def ensure_domain
-  return unless /\.herokuapp.com/ =~ request.host
-
-  # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
-  port = ":#{request.port}" unless [80, 443].include?(request.port)
-  redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
-  end
 end
