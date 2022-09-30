e5da1214c8b19e62432f87eb2b0909393aa448aa
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-09-30 12:27:12 +0900

    Fix:詳細画面のアウトプットの表示順を上から昇順に設定

--- app/controllers/api/outputs_controller.rb
+++ app/controllers/api/outputs_controller.rb
@@ -2,7 +2,7 @@ class Api::OutputsController < ApplicationController
   before_action :set_output, only: %i[update destroy]
 
   def show
-    @outputs = Output.where(video_id: params[:id]).includes(:user, :likes, :comments)
+    @outputs = Output.where(video_id: params[:id]).includes(:user, :likes, :comments).order(created_at: :asc)
     render json: @outputs.to_json(include: [:user, :likes, { comments: { include: :user } }])
   end
 
