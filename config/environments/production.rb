c76ecf34de8383888774edc4144772e36c072a28
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-10-06 08:20:37 +0900

    Fix:デプロイ時のsyntaxErrorの解消

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
     password: Rails.application.credentials.gmail[:password],
     #パスワードをBase64でエンコード
     authentication: :login
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
