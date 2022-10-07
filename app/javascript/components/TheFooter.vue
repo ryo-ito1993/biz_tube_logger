0b4294403c665267a0305d7a9f97846f3537ce1a
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-10-07 23:21:10 +0900

    Add:プライバシーポリシーページの実装、フッターにお問合せ(twitter)リンク設置

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -10,12 +10,22 @@
       >
         利用規約
       </v-btn>
+      <router-link :to="{ name: 'PrivacyPolicy' }">
       <v-btn
         dense
         text
       >
         プライバシーポリシー
       </v-btn>
+      </router-link>
+      <a href="https://twitter.com/ryo_runteq32" target="_blank">
+        <v-btn
+        dense
+        text
+      >
+        お問い合せ
+      </v-btn>
+      </a>
       <small>&copy; BizTubeLogger 2022.</small>
     </div>
   </v-footer>
@@ -35,5 +45,10 @@ small {
 .v-btn:not(.v-btn--round).v-size--default {
   padding: 0 5px;
 }
-
+a {
+  text-decoration: none;
+  }
+a:link, a:visited {
+  color: white;
+}
 </style>
