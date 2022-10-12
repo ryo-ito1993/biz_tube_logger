461fd0804c85ce8133329cc6b6031ec0597aebc8
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-10-12 16:49:46 +0900

    Add:output,category,commentのリクエストスペックの実装

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes", type: :request do
+RSpec.describe "Videos", type: :request do
   let(:video) { create(:video) }
   let(:category) { create(:category) }
   let(:user) { create(:user) }
@@ -9,6 +9,18 @@ RSpec.describe "Recipes", type: :request do
     it "投稿一覧を取得できる" do
       video
       get api_videos_path
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(json[0]['title']).to eq(video.title)
+      expect(json[0]['thumbnail']).to eq(video.thumbnail)
+      expect(json[0]['view_count']).to eq(video.view_count)
+    end
+  end
+
+  describe "GET /api/videos/display_videos" do
+    it "投稿一覧を取得できる" do
+      video
+      get display_videos_api_videos_path
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(json[0]['title']).to eq(video.title)
@@ -32,6 +44,18 @@ RSpec.describe "Recipes", type: :request do
       expect(json[0]['view_count']).to eq(Video.first.view_count)
       expect(json[1]['summary']).to eq(Output.first.summary)
       expect(json[1]['impression']).to eq(Output.first.impression)
+    end
+  end
+
+  describe "/api/videos/:id" do
+    it "video詳細を取得できる" do
+      video
+      get api_video_path(video.id)
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(json[0]['title']).to eq(video.title)
+      expect(json[0]['thumbnail']).to eq(video.thumbnail)
+      expect(json[0]['view_count']).to eq(video.view_count)
     end
   end
 end
