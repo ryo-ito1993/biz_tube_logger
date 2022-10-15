2b97439e98715401259361e6c403d9e79cf6a0df
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-10-15 21:26:25 +0900

    Add:schedule.rbにPATHの設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,6 +21,8 @@
 
 # Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + '/environment')
+# PATHの設定
+job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 # cronを実行する環境変数(RAILS_ENVが指定されていないときはdevelopmentを使用)
 rails_env = ENV['RAILS_ENV'] || :development
 # cronを実行する環境変数をセット（上記で作成した変数を指定）
