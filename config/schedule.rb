9065ccd635a5fc3e21f5816f2e1631387ef5e9fd
ryo-ito1993 <ryo.ito1993@gmail.com>
2022-10-16 09:54:04 +0900

    Add:schedule.rbにzshのPATH設定を追加

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,8 @@
 # Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 # PATHの設定
-job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+set :job_template, "/bin/zsh -l -c ':job'"
+job_type :rake, "source /Users/ryo/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 # cronを実行する環境変数(RAILS_ENVが指定されていないときはdevelopmentを使用)
 rails_env = ENV['RAILS_ENV'] || :development
 # cronを実行する環境変数をセット（上記で作成した変数を指定）
@@ -30,6 +31,6 @@ set :environment, rails_env
 # cronのログファイルの出力先指定
 set :output, "#{Rails.root}/log/cron.log"
 
-every :day do
+every :minute do
   rake 'view_count:update_view_count'
 end
